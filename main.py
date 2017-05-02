83de45f5c04d48ae300f6dc108b5947c85c05302
Isaac Daniel Batista <disa.indus@gmail.com>
2017-05-02 13:54:23 -0500

    Iniciando con Flask

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# @ signifies a decorator - way to wrap a function and modifying its behavior
+@app.route('/')
+def index():
+    return 'This is the home page!'
+
+@app.route('/tuna')
+def tuna():
+    return '<h2>Tuna is good!!</h2>'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return '<h2>Hey there %s</h2>' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return '<h2>Posy ID id %s</h2>' % post_id
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
