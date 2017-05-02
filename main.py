393e691a9e77d2db4ef4e0ed2206ae86ca6805aa
Isaac Daniel Batista <disa.indus@gmail.com>
2017-05-02 14:58:47 -0500

    Methods get and post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,7 +6,17 @@ app = Flask(__name__)
 # @ signifies a decorator - way to wrap a function and modifying its behavior
 @app.route('/')
 def index():
-    return 'This is the home page!'
+    return 'Method used: %s' % request.method
+
+@app.route('/bacon', methods=['GET', 'POST'])
+def bacon():
+    if request.method == 'POST':
+        return 'You are using POST'
+    else:
+        return 'You are probably using GET'
+# @app.route('/')
+# def index():
+#     return 'This is the home page!'
 
 @app.route('/tuna')
 def tuna():
@@ -19,8 +29,6 @@ def profile(username):
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return '<h2>Posy ID id %s</h2>' % post_id
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
